552787cb4a41560c879112f6be939177f461608c
VinC <vinc_bour@hotmail.com>
2020-05-25 22:14:15 -0400

    :fire: Remove and clean some files

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,37 +1,48 @@
-'use strict';
+'use strict'
 
-import * as functions from 'firebase-functions';
-
-const sgMail = require('@sendgrid/mail')
-
-sgMail.setApiKey('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')
+import * as functions from 'firebase-functions'
+const postmark: any = require('postmark')
 
 // const runtimeOpts = {
 //   timeoutSeconds: 500,
 //   memory: '1GB'
 // }
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
 exports.sendContactRequest = functions.firestore
   .document('contacts/{contactId}')
-  .onCreate((snap) => {
+  .onCreate((snap: functions.firestore.DocumentSnapshot): string => {
+
+    console.log('sendContactRequest has started!')
+
     if (snap && snap.data()) {
-      // @ts-ignore
-      const { email, name, body } = snap.data()
-      const msg = {
-        to: 'vbourdeau3@gmail.com',
-        from: email,
-        templateId: 'd-1c259b8d491643968dfddaa8a36c1eba',
-        dynamic_template_data: {
-          name,
+      // Send an email
+      const data: any = snap.data()
+      const client: any = new postmark.ServerClient('49586da6-0a85-48de-9159-20936a7a384d')
+      const { email, name, body } = data
+
+      console.log('Client', client)
+
+      client.sendEmailWithTemplate({
+        "From": "hosting+vb@showadz.io",
+        "To": "vbourdeau3@gmail.com",
+        "TemplateAlias": "vb-basic",
+        "TemplateModel": {
+          "product_url": "#",
+          "product_name": "VB",
           email,
-          body
+          name,
+          body,
+          "company_name": "",
+          "company_address": "",
+          "sender_name": "",
+          "help_url": ""
         }
-      }
-      return sgMail.send(msg)
+      })
+
+      return 'sent'
     } else {
-      return null
+      return 'null'
     }
 })
--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,52 @@
 <template>
   <div id="app">
-
     <!-- Main App Content -->
     <main role="main">
+      <router-view key="main" />
 
-        <router-view key="main" />
+      <!-- Overlays SVG for Home page -->
+      <div
+        v-if="$route.name === 'home'"
+        key="overlays"
+        class="overlays overlays-under"
+      >
+        <svg
+          class="left"
+          width="702"
+          height="777"
+          viewBox="0 0 702 777"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M602.5 326.469C492.1 178.857 142.167 49.4459 -9 0.174988V776.945C39.8333 749.964 200.724 667.093 370 684.437C792 727.675 740.5 510.983 602.5 326.469Z"
+          />
+        </svg>
 
-        <!-- Overlays SVG for Home page -->
-        <div key="overlays" v-if="$route.name === 'home'" class="overlays overlays-under">
-
-          <svg class="left" width="702" height="777" viewBox="0 0 702 777" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M602.5 326.469C492.1 178.857 142.167 49.4459 -9 0.174988V776.945C39.8333 749.964 200.724 667.093 370 684.437C792 727.675 740.5 510.983 602.5 326.469Z" />
-          </svg>
-
-          <svg class="right" width="340" height="1122" viewBox="0 0 340 1122" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M109.628 425.5C218.428 338.7 321.961 105.667 360.128 0V1121.5C302.528 1035.9 169.128 917.5 109.628 869C-46.3719 717 -26.3719 534 109.628 425.5Z"/>
-          </svg>
-
-        </div>
-
+        <svg
+          class="right"
+          width="340"
+          height="1122"
+          viewBox="0 0 340 1122"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M109.628 425.5C218.428 338.7 321.961 105.667 360.128 0V1121.5C302.528 1035.9 169.128 917.5 109.628 869C-46.3719 717 -26.3719 534 109.628 425.5Z"
+          />
+        </svg>
+      </div>
     </main>
 
     <!-- Contact Form Component -->
     <contact-form />
 
     <!-- Intro animation SVGs -->
-    <svg class="shape-overlays" viewBox="0 0 100 100" preserveAspectRatio="none">
+    <svg
+      class="shape-overlays"
+      viewBox="0 0 100 100"
+      preserveAspectRatio="none"
+    >
       <path class="shape-overlays__path"></path>
       <path class="shape-overlays__path"></path>
       <path class="shape-overlays__path"></path>
@@ -124,7 +145,7 @@ export default {
       position: absolute;
       top: 0;
       left: 0;
-      opacity: .80;
+      opacity: 0.8;
       fill: $accent2-color;
     }
   }
@@ -133,7 +154,7 @@ export default {
     position: absolute;
     top: 43%;
     right: 0;
-    opacity: .789;
+    opacity: 0.789;
     fill: #0066fb;
     -webkit-transform: scale(2.5);
     transform: scale(2.5);
@@ -142,7 +163,7 @@ export default {
       position: absolute;
       top: 0;
       right: 0;
-      opacity: .80;
+      opacity: 0.8;
       fill: $accent-color;
 
       transform: scale(2.5);
@@ -153,7 +174,7 @@ export default {
     position: absolute;
     top: 0;
     left: 0;
-    opacity: .25;
+    opacity: 0.25;
     fill: rgba(255, 226, 16, 0.54);
   }
 }
